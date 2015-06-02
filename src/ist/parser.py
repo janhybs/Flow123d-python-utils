f17665a9d583934f398a289546bdafaffd751ca6
Jan_Hybs <jan.hybs@tul.cz>
2015-06-02 15:24:03 +0200

    Uncomment json parsing and formatting

--- src/ist/parser.py
+++ src/ist/parser.py
@@ -2,35 +2,36 @@
 # author:   Jan Hybs
 
 #
-# import json
-# from ist.formatters.latex import LatexRecord, LatexFormatter
-# from ist.nodes import TypedList, Array
-# from ist.nodes import Record, AbstractRecord, Selection
-#
-#
-# class ProfilerJSONDecoder (json.JSONDecoder):
-# def decode (self, json_string):
-# default_obj = super (ProfilerJSONDecoder, self).decode (json_string)
-# lst = TypedList ()
-# lst.parse (default_obj)
-# return lst
-#
-#
-# json_location = 'example.json'
-# with open (json_location, 'r') as fp:
-# # parse json file
-# jsonObj = json.load (fp, encoding="utf-8", cls=ProfilerJSONDecoder)
-#
-# result = LatexFormatter.format (jsonObj)
-# result = ''.join(result)
-#
-# with open ('../../docs/input_reference_red.tex', 'w') as fp:
-# fp.write (result)
-#
-#
+import json
+from ist.formatters.json2latex import LatexRecord, LatexFormatter
+from ist.globals import Globals
+from ist.nodes import TypedList, Array
+from ist.nodes import Record, AbstractRecord, Selection
 
-from markdown import markdown
+
+class ProfilerJSONDecoder (json.JSONDecoder):
+    def decode (self, json_string):
+        default_obj = super (ProfilerJSONDecoder, self).decode (json_string)
+        lst = TypedList ()
+        lst.parse (default_obj)
+        return lst
+
+
+json_location = 'example.json'
+with open (json_location, 'r') as fp:
+    # parse json file
+    jsonObj = json.load (fp, encoding="utf-8", cls=ProfilerJSONDecoder)
+
+result = LatexFormatter.format (jsonObj)
+result = ''.join(result)
+
+with open ('../../docs/input_reference_red.tex', 'w') as fp:
+    fp.write (result)
+
+
+
 import xml.etree.ElementTree as ET
+import markdown
 from ist.formatters.html2latex import Html2Latex
 from ist.utils.texlist import texlist
 
@@ -58,8 +59,17 @@ x = 0
 x = 2 + 2
 what is x
 ```
+[[record_bar]]
+
+
+
 """
-html = markdown (latex, extensions=['markdown.extensions.sane_lists', 'markdown.extensions.nl2br'])
+html = markdown.markdown (latex, extensions=[
+    'markdown.extensions.sane_lists',
+    'markdown.extensions.nl2br',
+    'ist.formatters.extensions.flowlinks'
+
+])
 tree = ET.fromstring ('<html>' + html + "</html>")
 
 
@@ -68,3 +78,6 @@ tex = Html2Latex (tree).to_latex ()
 print tex
 print html
 print ''.join (tex)
+
+g = Globals
+print g
