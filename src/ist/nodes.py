2939d094a9e343602c516aba1538479c64ba3976
Jan_Hybs <jan.hybs@tul.cz>
2015-06-09 15:53:16 +0200

    Refactor code
     - add attributes to to json example

--- src/ist/nodes.py
+++ src/ist/nodes.py
@@ -1,10 +1,10 @@
 # encoding: utf-8
 # author:   Jan Hybs
 from ist.globals import Globals
-from ist.utils.utils import Field, TypedList
+from ist.utils.utils import Field, TypedList, AttributeDict
 
 
-class ISTNode (object):
+class ISTNode(object):
     # type of the object (similar to __class__.__name__)
     __type__ = ''
 
@@ -17,87 +17,87 @@ class ISTNode (object):
     _fields = []
 
 
-    def get_name (self):
-        return getattr (self, self.__name_field__)
+    def get_name(self):
+        return getattr(self, self.__name_field__)
 
 
-    def get_type (self):
+    def get_type(self):
         return self.__type__
 
 
-    def get_value (self):
-        return getattr (self, self.__value_field__)
+    def get_value(self):
+        return getattr(self, self.__value_field__)
 
-    def parse (self, o={ }):
+    def parse(self, o={ }):
         for field in self._fields:
             # parse list
-            if issubclass (field.type, TypedList):
-                value = field.value.copy ().parse (o.get (field.name, []))
+            if issubclass(field.type, TypedList) or issubclass(field.type, AttributeDict):
+                value = field.value.copy().parse(o.get(field.name, []))
             # parse more complex objects
-            elif issubclass (field.type, ISTNode):
-                value = field.value.copy ().parse (o.get (field.name, { }))
+            elif issubclass(field.type, ISTNode):
+                value = field.value.copy().parse(o.get(field.name, { }))
             # simple values
             else:
-                value = o.get (field.name)
+                value = o.get(field.name)
 
             # create attribute on class instance
-            self.__setattr__ (field.name, value)
+            self.__setattr__(field.name, value)
             if field.name == 'id' and value is not None:
                 Globals.items[value] = self
 
         return self
 
-    def include_in_format (self):
+    def include_in_format(self):
         return True
 
-    def get (self, *args):
+    def get(self, *args):
         for arg in args:
-            value = getattr (self, arg, None)
+            value = getattr(self, arg, None)
             if value is not None:
                 return value
 
-        raise Exception ('no valid attribute within {} found on {}'.format (args, self.__class__.__name__))
+        raise Exception('no valid attribute within {} found on {}'.format(args, self.__class__.__name__))
 
 
-    def copy (self):
-        '''
+    def copy(self):
+        """
         Return copy of this instance
         :return:
-        '''
-        return self.__class__ ()
+        """
+        return self.__class__()
 
-    def __repr__ (self):
-        return '<{self.__class__.__name__} {self._fields}>'.format (self=self)
+    def __repr__(self):
+        return '<{self.__class__.__name__} {self._fields}>'.format(self=self)
 
 
-class Reference (ISTNode):
+class Reference(ISTNode):
     __type__ = 'Reference'
     __value_field__ = 'ref_id'
 
-    def parse (self, o=''):
+    def parse(self, o=''):
         self.ref_id = o
         return self
 
-    def get_reference (self):
-        return Globals.items.get (str (self.ref_id), None)
+    def get_reference(self):
+        return Globals.items.get(str(self.ref_id), None)
 
-    def copy (self):
-        return Reference ()
+    def copy(self):
+        return Reference()
 
-    def __repr__ (self):
-        return "<Reference ({self.ref_id}) -> {ref}>".format (self=self, ref=self.get_reference ())
+    def __repr__(self):
+        return "<Reference ({self.ref_id}) -> {ref}>".format(self=self, ref=self.get_reference())
 
-    def __nonzero__ (self):
-        return self.get_reference () is not None
+    def __nonzero__(self):
+        return self.get_reference() is not None
 
 
-class NumberRange (ISTNode):
+class NumberRange(ISTNode):
     __type__ = 'Range'
     __name_field__ = ''
     __value_field__ = ''
 
-    def __init__ (self, always_visible=True):
-        super (NumberRange, self).__init__ ()
+    def __init__(self, always_visible=True):
+        super(NumberRange, self).__init__()
         self.min = self.max = ''
         self.always_visible = always_visible
 
@@ -110,214 +110,230 @@ class NumberRange (ISTNode):
         '': 'unknown range'
     }
 
-
-    def parse (self, o=[]):
-        self.min = o[0] if len (o) > 0 else ''
-        self.max = o[1] if len (o) > 1 else ''
+    def parse(self, o=[]):
+        self.min = o[0] if len(o) > 0 else ''
+        self.max = o[1] if len(o) > 1 else ''
 
         return self
 
-    def is_pointless (self):
-        '''
+    def is_pointless(self):
+        """
         Wheather is information within this instance beneficial
-        '''
-        return self._format () in ('[0, ]', '[, ]')
+        """
+        return self._format() in ('[0, ]', '[, ]')
 
-    def _format (self):
-        '''
+    def _format(self):
+        """
         Method will will return string representation of this instance
         :return:
-        '''
-        return '[{}, {}]'.format (
-            self.replacements.get (str (self.min), self.min),
-            self.replacements.get (str (self.max), self.max)
+        """
+        return '[{}, {}]'.format(
+            self.replacements.get(str(self.min), self.min),
+            self.replacements.get(str(self.max), self.max)
         )
 
-    def copy (self):
-        return NumberRange (self.always_visible)
+    def copy(self):
+        return NumberRange(self.always_visible)
 
-    def __repr__ (self):
-        '''
+    def __repr__(self):
+        """
         method will return string representation if is meaningful or flag always visible
         is True
         :return:
-        '''
-        return self._format () if self.always_visible or not self.is_pointless () else ''
+        """
+        return self._format() if self.always_visible or not self.is_pointless() else ''
 
 
-class DoubleRange (NumberRange):
+class DoubleRange(NumberRange):
     __type__ = 'Range'
     __name_field__ = ''
 
-    def __init__ (self, always_visible=False):
-        super (DoubleRange, self).__init__ (always_visible)
+    def __init__(self, always_visible=False):
+        super(DoubleRange, self).__init__(always_visible)
 
-    def is_pointless (self):
-        return self._format () == '[, ]'
+    def is_pointless(self):
+        return self._format() == '[, ]'
 
-    def copy (self):
-        return DoubleRange (self.always_visible)
+    def copy(self):
+        return DoubleRange(self.always_visible)
 
 
-class AbstractNode (ISTNode):
+class AbstractNode(ISTNode):
     _fields = ISTNode._fields + [
-        Field ('id'),
-        Field ('input_type')
+        Field('id'),
+        Field('input_type')
     ]
 
 
-class DescriptionNode (AbstractNode):
-    _fields = AbstractNode._fields + [
-        Field ('description')
-    ]
-
-
-class RecordKeyDefault (AbstractNode):
-    __type__ = 'Defaults'
-    __name_field__ = ''
+class Attribute(AbstractNode):
+    __type__ = 'Attribute'
+    __name_field__ = 'name'
     __value_field__ = 'value'
 
     _fields = AbstractNode._fields + [
-        Field ('type'),
-        Field ('value')
+        Field('name'),
+        Field('value')
     ]
 
 
-class RecordKey (DescriptionNode):
-    __type__ = 'Record key'
-    __name_field__ = 'key'
-    __value_field__ = 'default'
+class DescriptionNode(AbstractNode):
+    _fields = AbstractNode._fields + [
+        Field('description')
+    ]
 
+
+class ComplexNode(DescriptionNode):
     _fields = DescriptionNode._fields + [
-        Field ('key'),
-        Field ('type', Reference ()),
-        Field ('default', RecordKeyDefault ())
-    ]
-
-    def include_in_format (self):
-        return self.key.find ('TYPE') == -1
-
-
-class Record (DescriptionNode):
-    __type__ = 'Record'
-    __name_field__ = 'type_name'
-    __value_field__ = 'type_name'
-
-    _fields = DescriptionNode._fields + [
-        Field ('type_name'),
-        Field ('input_type'),
-        Field ('type_full_name'),
-        Field ('keys', TypedList (RecordKey)),
-        Field ('implements', TypedList (Reference)),
-        Field ('reducible_to_key')
+        Field('attributes', AttributeDict(Attribute))
     ]
 
 
-class AbstractRecord (Record):
-    __type__ = 'AbstractRecord'
-    __name_field__ = 'name'
-    __value_field__ = 'name'
-
-    _fields = Record._fields + [
-        Field ('implementations', TypedList (Reference)),
-        Field ('default_descendant', Reference ()),
-        Field ('full_name'),
-        Field ('name')
-    ]
-
-
-class SelectionValue (DescriptionNode):
+class SelectionValue(DescriptionNode):
     __type__ = 'Selection value'
     __name_field__ = 'name'
     __value_field__ = 'name'
 
     _fields = DescriptionNode._fields + [
-        Field ('name')
+        Field('name')
     ]
 
 
-class Selection (DescriptionNode):
-    __type__ = 'Selection'
-    __name_field__ = 'name'
-    __value_field__ = 'name'
+class RecordKeyDefault(AbstractNode):
+    __type__ = 'Defaults'
+    __name_field__ = ''
+    __value_field__ = 'value'
+
+    _fields = AbstractNode._fields + [
+        Field('type'),
+        Field('value')
+    ]
+
+
+class RecordKey(DescriptionNode):
+    __type__ = 'Record key'
+    __name_field__ = 'key'
+    __value_field__ = 'default'
 
     _fields = DescriptionNode._fields + [
-        Field ('values', TypedList (SelectionValue)),
-        Field ('name'),
-        Field ('full_name')
+        Field('key'),
+        Field('type', Reference()),
+        Field('default', RecordKeyDefault())
     ]
 
-    def include_in_format (self):
-        return self.name.find ('TYPE') == -1
+    def include_in_format(self):
+        return self.key.find('TYPE') == -1
 
 
-class String (AbstractNode):
+class String(AbstractNode):
     __type__ = 'String'
     __name_field__ = 'name'
     __value_field__ = 'name'
 
     _fields = AbstractNode._fields + [
-        Field ('name'),
-        Field ('full_name')
+        Field('name'),
+        Field('full_name')
     ]
 
 
-class Double (AbstractNode):
+class Double(AbstractNode):
     __type__ = 'Double'
     __name_field__ = 'name'
     __value_field__ = 'range'
 
     _fields = AbstractNode._fields + [
-        Field ('name'),
-        Field ('full_name'),
-        Field ('range', DoubleRange ())
+        Field('name'),
+        Field('full_name'),
+        Field('range', DoubleRange())
     ]
 
 
-class Integer (AbstractNode):
+class Integer(AbstractNode):
     __type__ = 'Integer'
     __name_field__ = 'name'
     __value_field__ = 'range'
 
     _fields = AbstractNode._fields + [
-        Field ('name'),
-        Field ('full_name'),
-        Field ('range', NumberRange ())
+        Field('name'),
+        Field('full_name'),
+        Field('range', NumberRange())
     ]
 
 
-class FileName (AbstractNode):
+class FileName(AbstractNode):
     __type__ = 'FileName'
     __name_field__ = 'name'
     __value_field__ = 'file_mode'
 
     _fields = AbstractNode._fields + [
-        Field ('name'),
-        Field ('full_name'),
-        Field ('file_mode')
+        Field('name'),
+        Field('full_name'),
+        Field('file_mode')
     ]
 
 
-class Bool (AbstractNode):
+class Bool(AbstractNode):
     __type__ = 'Bool'
     __name_field__ = 'name'
     __value_field__ = ''
 
     _fields = AbstractNode._fields + [
-        Field ('name'),
-        Field ('full_name')
+        Field('name'),
+        Field('full_name')
     ]
 
 
-class Array (AbstractNode):
+class Array(AbstractNode):
     __type__ = 'Array'
     __name_field__ = 'name'
     __value_field__ = 'range'
 
     _fields = AbstractNode._fields + [
-        Field ('range', NumberRange (False)),
-        Field ('subtype', Reference ())
+        Field('range', NumberRange(False)),
+        Field('subtype', Reference())
     ]
+
+
+class Record(ComplexNode):
+    __type__ = 'Record'
+    __name_field__ = 'type_name'
+    __value_field__ = 'type_name'
+
+    _fields = ComplexNode._fields + [
+        Field('type_name'),
+        Field('input_type'),
+        Field('type_full_name'),
+        Field('keys', TypedList(RecordKey)),
+        Field('implements', TypedList(Reference)),
+        Field('reducible_to_key')
+    ]
+
+
+class AbstractRecord(ComplexNode):
+    __type__ = 'AbstractRecord'
+    __name_field__ = 'name'
+    __value_field__ = 'name'
+
+    _fields = Record._fields + [
+        Field('implementations', TypedList(Reference)),
+        Field('default_descendant', Reference()),
+        Field('full_name'),
+        Field('name')
+    ]
+
+
+class Selection(ComplexNode):
+    __type__ = 'Selection'
+    __name_field__ = 'name'
+    __value_field__ = 'name'
+
+    _fields = ComplexNode._fields + [
+        Field('values', TypedList(SelectionValue)),
+        Field('name'),
+        Field('full_name')
+    ]
+
+    def include_in_format(self):
+        return self.name.find('TYPE') == -1
 
 
 # all acceptable input_type
--- src/ist/utils/utils.py
+++ src/ist/utils/utils.py
@@ -4,42 +4,57 @@
 
 
 
-class Field (object):
-    def __init__ (self, name, value=None, type=str):
+class Field(object):
+    def __init__(self, name, value=None, type=str):
         self.name = name
         self.value = value
         self.type = type if value is None else value.__class__
 
-    def __repr__ (self):
-        if issubclass (self.type, TypedList):
-            return "<'{self.name}[{self.type}]'>".format (self=self)
-        return "<'{self.name}'>".format (self=self)
+    def __repr__(self):
+        if issubclass(self.type, TypedList):
+            return "<'{self.name}[{self.type}]'>".format(self=self)
+        return "<'{self.name}'>".format(self=self)
 
 
-class TypedList (list):
-    def __init__ (self, cls=None, *args, **kwargs):
+class TypedList(list):
+    def __init__(self, cls=None, *args, **kwargs):
         self.cls = cls
-        list.__init__ (self, *args, **kwargs)
+        list.__init__(self, *args, **kwargs)
 
-    def parse (self, lst):
+    def parse(self, lst):
         from ist.nodes import registered_nodes
 
         if self.cls is None:
             for o in lst:
-                input_type = o.get ('input_type', '')
+                input_type = o.get('input_type', '')
                 found = False
-                for node_type, cls in registered_nodes.iteritems ():
+                for node_type, cls in registered_nodes.iteritems():
                     if input_type == node_type:
-                        self.append (cls ().parse (o))
+                        self.append(cls().parse(o))
                         found = True
                         break
 
                 if not found:
-                    self.append (o)
+                    self.append(o)
         else:
             for o in lst:
-                self.append (self.cls ().parse (o))
+                self.append(self.cls().parse(o))
         return self
 
-    def copy (self):
-        return TypedList (self.cls)
+    def copy(self):
+        return TypedList(self.cls)
+
+
+class AttributeDict(dict):
+    def __init__(self, cls=None, *args, **kwargs):
+        self.cls = cls
+        dict.__init__(self, *args, **kwargs)
+
+    def parse(self, lst):
+        for attribute in lst:
+            self[attribute['name']] = self.cls().parse(attribute) if self.cls else attribute
+
+        return self
+
+    def copy(self):
+        return AttributeDict(self.cls)
