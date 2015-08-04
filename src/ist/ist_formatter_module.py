5eabd61832e195fce2eda47680ea50b6d39a907a
Jan_Hybs <jan.hybs@tul.cz>
2015-08-04 14:54:32 +0200

    Add option to focus one html element and hide specific parts of page

--- src/ist/ist_formatter_module.py
+++ src/ist/ist_formatter_module.py
@@ -3,11 +3,9 @@
 
 #
 import json
-from ist.formatters.extensions.md_latex import MdLatexSupport
 
 from ist.formatters.json2html import HTMLFormatter
 from ist.formatters.json2latex import LatexFormatter
-from ist.formatters.markdown2html import markdown2html
 from ist.nodes import TypedList
 from ist.utils.htmltree import htmltree
 
@@ -19,11 +17,13 @@ class ProfilerJSONDecoder(json.JSONDecoder):
         lst.parse(default_obj)
         return lst
 
+
 class ISTFormatter (object):
     """
     Class for formatting json to other formats
     """
-    def json2latex(self, input_file='examples/example.json', output_file='../../docs/input_reference_red.tex'):
+    @staticmethod
+    def json2latex(input_file='examples/example.json', output_file='../../docs/input_reference_red.tex'):
         """
         Method converts given json file to latex output file
         :param input_file:
@@ -38,11 +38,15 @@ class ISTFormatter (object):
             fp.write(latex_result)
 
 
-    def json2html(self, input_file='examples/example.json', output_file='../../docs/index.html'):
+    @staticmethod
+    def json2html(input_file, output_file, focus_element_id='root', skip_block_creation=[]):
         """
         Method converts given input file to single html output file
-        :param input_file:
-        :param output_file:
+        :param input_file:  input json file
+        :param output_file: output html file
+        :param focus_element_id: id of element which will be visible, default root
+        :param skip_block_creation: list of items which won't be created:
+         [title, button-control, left-list, ist, right-list]
         :return:
         """
         with open(input_file, 'r') as fp:
@@ -55,38 +59,59 @@ class ISTFormatter (object):
         html_body = htmltree('body')
         with html_body.open('div', '', { 'class': 'jumbotron' }):
             with html_body.open('div', '', { 'class': 'container' }):
-                with html_body.open('h1', 'Flow123d '):
-                    html_body.tag('small', 'input reference')
 
-                with html_body.open('div', attrib={ 'class': 'btn-group', 'data-toggle': 'buttons' }):
-                    btn_cls = dict({ 'class': 'btn btn-default btn-filter' })
-                    btn_cls['data-type'] = 'record'
-                    html_body.tag('a', 'Records', btn_cls)
-                    btn_cls['data-type'] = 'abstract-record'
-                    html_body.tag('a', 'Abstract records', btn_cls)
-                    btn_cls['data-type'] = 'selection'
-                    html_body.tag('a', 'Selections', btn_cls)
+                if 'title' not in skip_block_creation:
+                    with html_body.open('h1', 'Flow123d '):
+                        html_body.tag('small', 'input reference')
 
-                html_body.tag('a', 'Filter one', attrib={ 'class': 'btn btn-default btn-filter-one' })
+                if 'button-control' not in skip_block_creation:
+                    with html_body.open('div', attrib={'id': 'btn-filter-one-wrapper'}):
+                        html_body.tag('input', '', attrib={
+                            'type': 'checkbox',
+                            'class': 'btn btn-default',
+                            'id': ' btn-filter-one',
+                            'data-toggle': 'toggle',
+                            'data-on': 'Single-item',
+                            'data-off': 'Multi-item'
+                            })
+                    with html_body.open('div', attrib={ 'class': 'btn-group filter-btns', 'data-toggle': 'buttons' }):
+                        btn_cls = dict({ 'class': 'btn btn-default btn-filter' })
+                        btn_cls['data-type'] = 'record'
+                        html_body.tag('a', 'Records', btn_cls)
+                        btn_cls['data-type'] = 'abstract-record'
+                        html_body.tag('a', 'Abstract records', btn_cls)
+                        btn_cls['data-type'] = 'selection'
+                        html_body.tag('a', 'Selections', btn_cls)
 
-                with html_body.open('div', attrib={ 'class': 'col-md-2 tree-list' }):
-                    html_body.add(html_nav_tree.current())
-                with html_body.open('div', attrib={ 'class': 'col-md-8 input-reference' }):
-                    html_body.add(html_content.current())
-                    first_record = html_body.current()._children[0]._children[0]
-                    first_record.attrib['class'] = first_record.attrib['class'].replace('hidden', '')
-                with html_body.open('div', attrib={ 'class': 'col-md-2 abc-list' }):
-                    html_body.add(html_nav_abc.current())
+                if 'left-list' not in skip_block_creation:
+                    with html_body.open('div', attrib={ 'class': 'col-md-2 tree-list' }):
+                        html_body.add(html_nav_tree.current())
+
+                if 'ist' not in skip_block_creation:
+                    with html_body.open('div', attrib={ 'class': 'col-md-8 input-reference' }):
+                        html_body.add(html_content.current())
+
+                        # show specified element by given id
+                        for child in html_body.current()._children[0]:
+                            if child.attrib['id'].lower() == focus_element_id.lower():
+                                child.attrib['class'] = child.attrib['class'].replace('hidden', '')
+                            # print child.attrib['id'], child.attrib['class']
+
+                if 'right-list' not in skip_block_creation:
+                    with html_body.open('div', attrib={ 'class': 'col-md-2 abc-list' }):
+                        html_body.add(html_nav_abc.current())
 
         html_head = htmltree('head')
 
         html_head.tag('title', 'Flow123d input reference')
         html_head.style('css/main.css')
         html_head.style('css/bootstrap.min.css')
+        html_head.style('css/bootstrap-toggle.min.css')
         html_head.style('css/katex.min.css')
 
         html_body.script('js/jquery-2.1.3.min.js')
         html_body.script('js/bootstrap.min.js')
+        html_body.script('js/bootstrap-toggle.min.js')
         html_body.script('js/katex.min.js')
         html_body.script('js/main.js')
 
@@ -100,9 +125,15 @@ class ISTFormatter (object):
 
 
 if __name__ == '__main__':
-    print 'converting'
     formatter = ISTFormatter ()
     formatter.json2latex(
-        input_file='/home/jan-hybs/Dokumenty/Smartgit-flow/flow123d/doc/reference_manual/input_reference.json',
+        input_file='/home/jan-hybs/Dokumenty/Flow123d-python-utils/src/ist/examples/example.json',
         output_file='/home/jan-hybs/Dokumenty/Smartgit-flow/flow123d/doc/reference_manual/input_reference.tex'
+    )
+
+    formatter.json2html(
+        input_file='/home/jan-hybs/Dokumenty/Flow123d-python-utils/src/ist/examples/example.json',
+        output_file='/home/jan-hybs/Dokumenty/Smartgit-flow/flow123d/doc/reference_manual/input_reference.html',
+        focus_element_id='root',
+        skip_block_creation=['title','left-list', 'right-list']
     )
