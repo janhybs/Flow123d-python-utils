31b07d356e9523c9d55be31af2315cad692b6ca9
Jan_Hybs <jan.hybs@tul.cz>
2016-09-30 18:07:39 +0200

    Add artifact collection system

--- src/scripts/artifacts/collector.py
+++ src/scripts/artifacts/collector.py
@@ -1,61 +1,86 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 # author:   Jan Hybs
-
-import yaml
 import os
 import shutil
+
 import formic
 
-
-class CopyRule(object):
-    def __init__(self, source, target):
-        self.source = source
-        self.target = target
-
-    def __repr__(self):
-        return 'CopyRule({self.source} => {self.target})'.format(self=self)
-
-    def copy(self):
-        if not os.path.exists(os.path.dirname(self.target)):
-            os.makedirs(os.path.dirname(self.target))
-
-        shutil.copy(self.source, self.target)
+from scripts.artifacts.artifacts import ArtifactStep
+from scripts.core.base import Paths, Printer
 
 
-class YAMLTag(yaml.YAMLObject):
-    def __setstate__(self, state):
-        """ :type state: dict """
-        self.__init__(**state)
+class Collector(ArtifactStep):
+    """
+    Class Collector is simple class which collects files from certain
+    directory and copies them to specific output directory
+    """
 
-
-class Collector(YAMLTag):
     yaml_tag = u'!Collector'
 
-    def __init__(self, source=None, target=None, includes='*', excludes=None, flat=False, **kwargs):
+    def __init__(self, source=None, target=None, includes='*', excludes=None, flat=False, name=None, remove_original=False, **kwargs):
         self.source = source
         self.target = target
         self.includes = includes
         self.excludes = excludes
         self.flat = flat
+        self.name = name
+        self.remove_original = remove_original
         self.__dict__.update(kwargs)
+
+    @staticmethod
+    def create_path_dict(filename):
+        path = Paths.split(filename)[1:-1]
+        result = dict()
+        for i in range(len(path)):
+            result['+' + str(i)] = path[i]
+            result['-' + str(i)] = path[-i]
+        return result
 
     def __iter__(self):
         fileset = formic.FileSet(self.includes, directory=self.source)
         for filename in fileset:
+
+            name = Paths.basename(filename) if not self.name else self.name.format(
+                path=self.create_path_dict(filename),
+                name=Paths.basename(filename),
+            )
             if self.flat:
-                yield CopyRule(filename, os.path.join(self.target, os.path.basename(filename)))
+                root = self.target
             else:
-                target_dest = os.path.relpath(filename, os.path.abspath(self.source))
-                target_dest = os.path.abspath(os.path.join(self.target, target_dest))
-                yield CopyRule(filename, target_dest)
+                rel_path = Paths.relpath(Paths.dirname(filename), Paths.abspath(self.source))
+                root = Paths.abspath(Paths.join(self.target, rel_path))
+
+            yield CopyRule(filename, Paths.join(root, name), self.remove_original)
+
+    def run(self):
+        total = 0
+        for p in self:
+            total += 1
+            p.copy()
+        Printer.all.out("Copied out {} files", total)
+
+
+class CopyRule(object):
+    """
+    Class CopyRule represents action to copy single file to certain destination
+    """
+
+    def __init__(self, source, target, remove_original):
+        self.source = source
+        self.target = target
+        self.remove_original = remove_original
 
     def __repr__(self):
-        return 'Collector({self.source} => {self.target})'.format(self=self)
+        return 'CopyRule({self.source} => {self.target})'.format(self=self)
 
+    def copy(self):
+        # create dirs for target file
+        Paths.ensure_path(self.target)
 
-with open('artifact.yml', 'r') as fp:
-    configuration = yaml.load(fp)
-    for col in configuration['collectors']:
-        for rule in col:
-            print rule.copy()
+        # copy file
+        shutil.copy(self.source, self.target)
+
+        # remove file if set
+        if self.remove_original:
+            os.unlink(self.source)
